ff751b7a434e011e1f3baa52b027932ab11e4bc6
Valentine Baraka <96381376+vamreala@users.noreply.github.com>
2023-01-26 11:08:16 +0300

    Update functions1.c

--- functions1.c
+++ functions1.c
@@ -95,3 +95,14 @@ int print_hexadecimal(va_list types, char buffer[],
 		flags, 'x', width, precision, size));
 }
 
+/************* PRINT UNSIGNED NUMBER IN UPPER HEXADECIMAL **************/
+/**
+ * print_hexa_upper - Prints an unsigned number in upper hexadecimal notation
+ * @types: Lista of arguments
+ * @buffer: Buffer array to handle print
+ * @flags:  Calculates active flags
+ * @width: get width
+ * @precision: Precision specification
+ * @size: Size specifier
+ * Return: Number of chars printed
+ */
