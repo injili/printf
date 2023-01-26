cf7e9410615c6992ca23428763374263f2b3373c
injili <nyarekigospel@gmail.com>
2023-01-26 08:16:37 -0800

    the second function file

--- functions2.c
+++ functions2.c
@@ -1,6 +1,6 @@
 #include "main.h"
 
-/****************** PRINT POINTER ******************/
+
 /**
  * print_pointer - Prints the value of a pointer variable
  * @types: List a of arguments
@@ -9,6 +9,7 @@
  * @width: get width
  * @precision: Precision specification
  * @size: Size specifier
+ *
  * Return: Number of chars printed.
  */
 int print_pointer(va_list types, char buffer[],
@@ -52,7 +53,6 @@ if (addrs == NULL)
 		width, flags, padd, extra_c, padd_start));
 }
 
-/************************* PRINT NON PRINTABLE *************************/
 /**
  * print_non_printable - Prints ascii codes in hexa of non printable chars
  * @types: Lista of arguments
@@ -61,6 +61,7 @@ if (addrs == NULL)
  * @width: get width
  * @precision: Precision specification
  * @size: Size specifier
+ *
  * Return: Number of chars printed
  */
 int print_non_printable(va_list types, char buffer[],
@@ -92,7 +93,6 @@ int print_non_printable(va_list types, char buffer[],
 	return (write(1, buffer, i + offset));
 }
 
-/************************* PRINT REVERSE *************************/
 /**
  * print_reverse - Prints reverse string.
  * @types: Lista of arguments
@@ -101,6 +101,7 @@ int print_non_printable(va_list types, char buffer[],
  * @width: get width
  * @precision: Precision specification
  * @size: Size specifier
+ *
  * Return: Numbers of chars printed
  */
 
@@ -135,7 +136,7 @@ int print_reverse(va_list types, char buffer[],
 	}
 	return (count);
 }
-/************************* PRINT A STRING IN ROT13 *************************/
+
 /**
  * print_rot13string - Print a string in rot13.
  * @types: Lista of arguments
